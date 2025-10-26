eac018bf96dc77d07b470b77dee105837652ab7a
JunaidKhan19 <khanjunaid1719@gmail.com>
2025-10-26 19:48:54 +0530

    Add project files.

---
+++ day01/BasicClassConcepts/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BasicClassConcepts
+{
+    //public class Class1
+    //{
+    //    public int i;
+    //    public void Display()
+    //    {
+    //        System.Console.WriteLine("Display called");
+    //    }
+    //}
+
+    public class Class1
+    {
+        private int i;
+
+        public int I
+        {
+            set
+            {
+                if (value <= 100) i = value;
+                else System.Console.WriteLine("not a valid value");
+            }
+            get 
+            {  
+                return i; 
+            }
+        }
+        public void Display()
+        {
+            System.Console.WriteLine("Display called");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            ////System.Console.WriteLine("Junaid");
+            //Class1 obj1;
+            //obj1 = new Class1();
+            //obj1.Display();
+            //obj1.i=1;
+
+            //System.Console.WriteLine(obj1.i);
+
+        }
+    }
+}
---
+++ day01/assignment1/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace assignment1
+{
+    public class Employee
+    {
+        private string name;
+        private int empNo;
+        private decimal basic;
+        private short deptNo;
+        public string Name
+        {
+            set
+            {
+                if (value != "" && value != null)
+                {
+                    name = value;
+                }
+                else System.Console.WriteLine("Dont enter the empty name");
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+        public int EmpNo
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    empNo = value;
+                }
+                else System.Console.WriteLine("enter value above 0 for employee");
+            }
+            get
+            {
+                return empNo;
+            }
+        }
+
+        public decimal Basic
+        {
+            set
+            {
+                if (value > 0 && value <= 50000)
+                {
+                    basic = value;
+                }
+                else System.Console.WriteLine("enter value between 0 - 50000");
+            }
+            get
+            {
+                return basic;
+            }
+        }
+
+        public short DeptNo
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    deptNo = value;
+                }
+                else System.Console.WriteLine("enter value above 0");
+            }
+            get
+            {
+                return deptNo;
+            }
+        }
+
+        public decimal GetNetSalary()
+        {
+            decimal netSal = basic - (basic * 10 / 100);
+            return netSal; 
+        } 
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp;
+            emp = new Employee();
+            emp.Name = "Junaid";
+            emp.DeptNo = 1;
+            emp.EmpNo = 1;
+            emp.Basic = 10220;
+            System.Console.WriteLine("Name: "+ emp.Name);
+            System.Console.WriteLine("Department.No: " + emp.DeptNo);
+            System.Console.WriteLine("Emp.No: "+ emp.EmpNo);
+            System.Console.WriteLine("Basic salary: " + emp.Basic);
+            System.Console.WriteLine("Net Salary: " + emp.GetNetSalary());
+        }
+    }
+}
