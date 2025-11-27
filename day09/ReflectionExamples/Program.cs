a5f2fc0ed43525102dcf86a250df07e94fe87428
JunaidKhan19 <khanjunaid1719@gmail.com>
2025-11-27 16:27:06 +0530

    Reflection and its use

---
+++ day09/ReflectionExamples/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Junaid\Desktop\Github uploads\C#DotNet\day09\MyMathFunctions\bin\Debug\net9.0\MyMathFunctions.dll";
+
+            Assembly assembly = Assembly.LoadFrom(filePath);
+
+            Type[] mathType = assembly.GetTypes();
+
+            for (int i = 0; i < mathType.Length; i++)
+            {
+                Type type = mathType[i];
+
+                object? dynamicallyCreatedObject = assembly.CreateInstance(type.FullName);
+
+                MethodInfo[] methods = type.GetMethods();
+
+                for (int j = 0; j < methods.Length; j++)
+                {
+                    string methodSignature = "";
+                    MethodInfo currentMethod = methods[j];
+
+                    methodSignature += currentMethod.ReturnType.ToString() + " " + currentMethod.Name + " (";
+
+                    ParameterInfo[] parameters = currentMethod.GetParameters();
+
+                    for (int k = 0; k < parameters.Length; k++)
+                    {
+                        ParameterInfo currentParameter = parameters[k];
+
+                        methodSignature = methodSignature + " " + currentParameter.ParameterType.ToString() + " " + currentParameter.Name + ",";
+                    }
+
+                    object[] userInputs = new object[parameters.Length];
+                    for (int l = 0; l < parameters.Length; l++)
+                    {
+                        ParameterInfo param = parameters[l];
+                        Console.WriteLine($"Enter value for {param.Name} of type {param.ParameterType.ToString()}");
+                        object? value = Convert.ChangeType(Console.ReadLine(), param.ParameterType);
+                        userInputs[l] = value;
+                    }
+
+                    methodSignature = methodSignature.TrimEnd(',', ' ') + ")";
+
+                    Console.WriteLine(methodSignature);
+
+                    object result = type.InvokeMember(currentMethod.Name, BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, dynamicallyCreatedObject, userInputs); 
+
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
+02
---
+++ day09/ReflectionExplained/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ReflectionExplained
+{
+    internal class Program
+    {
+        //Reflection is reading metadat of an assembly at runtime.
+        //it is the ability of a program to examine and modify its own structure and behavior at runtime.
+        //usage scenarios:
+        //1. Displaying the information what the assembly contains.
+        //2. Reading assembly at suntime and creating an object of one of its classes in the assembly.
+        //   It allows us to dynamically load assemblies and call code on without giving reference at compile time.
+        //   It means to look at contents of assembly, create objects dynamically, call a method of that 
+        //   assembly at runtime without giving reference at compile time.
+        //3. reflection.emit: it is used to generate(create) a new assembly at runtime.
+        static void Main(string[] args)
+        {
+            //starting to read an object of assembly, the topmost object is Assembly
+
+            //Assembly.GetAssembly(typeof(Program)); //get the assembly in which Program class is defined
+            //Assembly.ExecutingAssembly(); //get the currently executing assembly
+            //Assembly.GetCallingAssembly(); //get the assembly of the method that called the currently executing method
+            //Assembly.GetEntryAssembly(); //get the entry assembly, the assembly that contains the topmost calling entry point.
+
+            //Assembly.Load("assemblyName"); //load an assembly by its name
+            //Assembly.LoadFrom("path of assembly"); //load an assembly from a given path
+
+            //load an assembly from a given file
+            try
+            {
+                string path = @"C:\Users\Junaid\Desktop\Github uploads\C#DotNet\day06\LinQSampleQuestions\bin\Debug\net9.0\LinQSampleQuestions.dll";
+
+                // Load the assembly
+                Assembly assem = Assembly.LoadFile(path);
+
+                // Display assembly metadata
+                Console.WriteLine("Assembly Full Name:");
+                Console.WriteLine(assem.FullName);
+
+                Console.WriteLine("\nTypes in this assembly:");
+                foreach (Type t in assem.GetTypes())
+                {
+                    Console.WriteLine(" - " + t.FullName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+}
